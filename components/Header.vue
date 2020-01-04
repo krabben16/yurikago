2b0637f9ed8b0f6d16184abb3955026be2a3141e
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-01-04 17:42:59 +0900

    add Nuxtでリファクタリングしたプロジェクトを追加

---
+++ components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <h1><nuxt-link to="/">Yurikago Blog</nuxt-link></h1>
+  </header>
+</template>
--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-
-import "prismjs/themes/prism-okaidia.css"
-import './src/css/gatsby-code-title.css';
