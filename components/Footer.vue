0e6c6ac08bbb8867d1e8dab2a543507612e6cb41
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-01-05 00:22:42 +0900

    add arigatoページを追加

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,10 +3,17 @@
     <hr>
     <div>
       <nuxt-link to="/about">About</nuxt-link>
+      <nuxt-link to="/arigato" class="arigato">&#x1f64f;</nuxt-link>
     </div>
     <div>ゆりかごから墓場まで</div>
     <div>
-      &copy; 2020 Hiroki Kawaguchi, Built with <a href="https://ja.nuxtjs.org" target="_blank">Nuxt.js</a>
+      &copy; 2020 Hiroki Kawaguchi, Built with <a href="https://ja.nuxtjs.org/" target="_blank">Nuxt.js</a>
     </div>
   </footer>
 </template>
+
+<style>
+.arigato {
+  float: right;
+}
+</style>
