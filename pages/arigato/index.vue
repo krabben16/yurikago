4bfc0285ef58872679707d8496b2f1044bbc46a1
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-01-05 00:22:42 +0900

    add arigatoページを追加

---
+++ pages/arigato/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      Favicon by <a href="https://icooon-mono.com/" target="_blank">ICOOON MONO</a>
+    </div>
+    <div>
+      CSS by <a href="https://github.com/simonlc/Markdown-CSS" target="_blank">simonlc/Markdown-CSS</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Arigato'
+    }
+  }
+}
+</script>
