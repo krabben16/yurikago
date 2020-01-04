b8a2454f39dd284c9a777c73efa5f73757b62fd1
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-01-04 17:42:59 +0900

    add Nuxtでリファクタリングしたプロジェクトを追加

---
+++ components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer>
+    <hr>
+    <div>
+      <nuxt-link to="/about">About</nuxt-link>
+    </div>
+    <div>ゆりかごから墓場まで</div>
+    <div>
+      &copy; 2020 Hiroki Kawaguchi, Built with <a href="https://ja.nuxtjs.org" target="_blank">Nuxt.js</a>
+    </div>
+  </footer>
+</template>
---
+++ pages/about/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-html="compiledMarkdown"></div>
+</template>
+
+<script>
+import marked from 'marked'
+
+import profile from '~/assets/markdown/about/profile.md'
+
+export default {
+  computed: {
+    compiledMarkdown () {
+      return marked(this.markdownText)
+    }
+  },
+  asyncData ({ context }) {
+    return {
+      markdownText: profile
+    }
+  },
+  head () {
+    return {
+      title: "About"
+    }
+  }
+}
+</script>
