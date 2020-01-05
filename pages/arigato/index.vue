435f157a084e4790522d75e0975640982c6dd6ba
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-01-05 19:36:52 +0900

    update arigatoページを更新

--- pages/arigato/index.vue
+++ pages/arigato/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
     <div>
-      Favicon by <a href="https://icooon-mono.com/" target="_blank">ICOOON MONO</a>
+      <h2>Markdown parser</h2>
+      <a href="https://marked.js.org/" target="_blank">Marked.js</a>
     </div>
     <div>
-      CSS by <a href="https://github.com/simonlc/Markdown-CSS" target="_blank">simonlc/Markdown-CSS</a>
+      <h2>Syntax highlighter</h2>
+      <a href="https://highlightjs.org/" target="_blank">highlight.js</a>
+    </div>
+    <div>
+      <h2>CSS</h2>
+      <a href="https://github.com/simonlc/Markdown-CSS" target="_blank">simonlc/Markdown-CSS</a>
+    </div>
+    <div>
+      <h2>Favicon</h2>
+      <a href="https://icooon-mono.com/" target="_blank">ICOOON MONO</a>
     </div>
   </div>
 </template>
