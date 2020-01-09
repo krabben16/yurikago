a81b92cc87423bb1314c0c2e8c50fa2cfd0ea7ed
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-01-09 10:24:27 +0900

    add ありがとうページのマークダウンを追加

---
+++ assets/markdown/arigato.md
@@ -0,0 +1,15 @@
+## Markdown parser
+
+[Marked.js](https://marked.js.org/)
+
+## Syntax highlighter
+
+[highlight.js](https://highlightjs.org/)
+
+## CSS library
+
+[Bootstrap](https://getbootstrap.com/)
+
+## Favicon
+
+[ICOOON MONO](https://icooon-mono.com/)
--- pages/arigato/index.vue
+++ pages/arigato/index.vue
@@ -1,22 +1,20 @@
 <template>
-  <div>
-    <div>
-      <h2>Markdown parser</h2>
-      <a href="https://marked.js.org/" target="_blank">Marked.js</a>
-    </div>
-    <div>
-      <h2>Syntax highlighter</h2>
-      <a href="https://highlightjs.org/" target="_blank">highlight.js</a>
-    </div>
-    <div>
-      <h2>Favicon</h2>
-      <a href="https://icooon-mono.com/" target="_blank">ICOOON MONO</a>
-    </div>
-  </div>
+  <Markdown :markdownContent="arigatoContent"></Markdown>
 </template>
 
 <script>
+import Markdown from '~/components/Markdown.vue'
+import content from '~/assets/markdown/arigato.md'
+
 export default {
+  components: {
+    Markdown
+  },
+  computed: {
+    arigatoContent () {
+      return content
+    }
+  },
   data () {
     return {
       title: 'ありがとうございます'
