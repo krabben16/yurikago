3b8af91c9cba9e97089b3907567e9f255a52f472
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-01-11 23:33:04 +0900

    fix DisableSanitizersの引数を修正

--- layouts/list.vue
+++ layouts/list.vue
@@ -41,16 +41,15 @@ export default {
     }
   },
   head () {
-    const uid = this._uid;
     return {
       // 構造化マークアップ
       script: [{
-        uid,
+        hid: 'breadcrumbSchema',
         innerHTML: this.$getBreadcrumbSchema(this.pageName, this.$route.path),
         type: 'application/ld+json'
       }],
       __dangerouslyDisableSanitizersByTagID: {
-        [uid]: 'innerHTML'
+        breadcrumbSchema: ['innerHTML']
       }
     }
   }
