b48d4db84adbc84cc062fec52984224de3b0df2c
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-01-20 20:45:04 +0900

    Fix axios 本番環境のベースURLを修正

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV == 'production' ? '' : 'http://localhost:3000'
+  baseURL: process.env.NODE_ENV == 'production' ? 'https://www.yurikago-blog.com' : 'http://localhost:3000'
 })
 
 export default ({ app }, inject) => {
