5b801b3a1af9793695f7d2d68df195795522c095
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-01-29 21:10:52 +0900

    Fix サムネイル画像を表示しないよう修正

--- components/Article.vue
+++ components/Article.vue
@@ -6,10 +6,6 @@
     @mouseleave="isActive = false"
     @click="changeRoute(article.id)"
   >
-    <picture v-if="article.article_thumbnail">
-      <source :srcset="article.article_thumbnail.src + '.webp'" type="image/webp">
-      <img class="card-img-top" :src="article.article_thumbnail.src + '.png'" :alt="article.article_thumbnail.alt">
-    </picture>
     <div class="card-body">
       <h5 class="card-title">{{ article.title }}</h5>
       <h6 class="card-subtitle text-muted">{{ article.posted_at }}</h6>
