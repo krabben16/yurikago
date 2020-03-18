16783cba5137901838d65cdcdc666fe5c0dd03db
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-03-18 20:58:22 +0900

    Fix ナビメニューにGitHubのリンクを表示する

--- components/Header.vue
+++ components/Header.vue
@@ -5,49 +5,27 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav mr-auto">
         <li class="nav-item">
           <nuxt-link :to="{ name: 'about' }" class="nav-link">About</nuxt-link>
         </li>
         <li class="nav-item">
           <nuxt-link :to="{ name: 'profile' }" class="nav-link">Profile</nuxt-link>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Tags
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <nuxt-link v-for="(tag, k) in tags" :key="k" class="dropdown-item" :to="{ name: 'articles-tag-id', params: { id: tag.id } }">
-              {{ tag.name }} ({{ tag.article_count }})
-            </nuxt-link>
-          </div>
-        </li>
         <li class="nav-item">
           <nuxt-link :to="{ name: 'arigato' }" class="nav-link">Arigato</nuxt-link>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a href="https://github.com/w9uZDFEYnT/yurikago" target="_blank">
+            <img src="/images/common/GitHub-Mark-32px.png" alt="GitHub" />
+          </a>
         </li>
       </ul>
     </div>
   </nav>
 </template>
-
-<script>
-export default {
-  methods: {
-    async fetchTags () {
-      const tags = await this.$axios.get('/tags')
-      this.tags = tags.data
-    }
-  },
-  data () {
-    return {
-      tags: null
-    }
-  },
-  mounted () {
-    this.fetchTags()
-  }
-}
-</script>
 
 <style lang="scss" scoped>
 nav {
