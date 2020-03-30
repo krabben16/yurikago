945f98edddf82605814c2661ad274aceac947d15
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-03-30 22:15:27 +0900

    fix githubのユーザー名を変更する

--- plugins/constant.js
+++ plugins/constant.js
@@ -3,7 +3,7 @@ import Vue from "vue"
 const constant = {
   FRONT_URL: "https://www.yurikago-blog.com",
   API_URL: process.env.NODE_ENV === "production" ? "https://api.yurikago-blog.com" : "http://192.168.10.10",
-  GITHUB_URL: "https://github.com/w9uZDFEYnT/yurikago",
+  GITHUB_URL: "https://github.com/krabben27/yurikago",
   DISQUS_SHORTNAME: "yurikago-blog",
   MAX_ARTICLE_COUNT_IN_LIST: 10,
   ARTICLE_AUTHOR: "Hiroki Kawaguchi",
