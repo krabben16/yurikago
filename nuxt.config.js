4447b4723dcb18ee45fa6621d900c89560b65388
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-04-01 20:21:38 +0900

    fix googleフォントの参照先を修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+JP:300&display=swap&subset=japanese' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap' },
       {
         rel: 'stylesheet',
         href: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
