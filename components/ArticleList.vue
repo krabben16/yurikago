8bd19f34a74d4cef157c7c6c5d55440e6ecb902d
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-07-12 01:52:31 +0900

    fix 不要なグリッドを削除する

--- components/ArticleList.vue
+++ components/ArticleList.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="container">
-    <div v-for="(article, i) in articles" :key="article.id" class="row">
-      <div class="col-12 col-sm-10 mx-sm-auto">
-        <!-- NOTE: 二個目の記事からマージンを設定する -->
-        <div :class="{ 'mt-5': i > 0 }">
-          <h5>
-            <nuxt-link :to="{ name: 'articles-id', params: { id: article.id } }" class="u-link-color">
-              {{ article.title }}
-            </nuxt-link>
-          </h5>
-          <p class="text-muted">{{ article.posted_at }}</p>
-        </div>
-      </div>
+  <div>
+    <!-- NOTE: 二個目の記事からマージンを設定する -->
+    <div v-for="(article, i) in articles" :key="article.id" :class="{ 'mt-5': i > 0 }">
+      <h5>
+        <nuxt-link :to="{ name: 'articles-id', params: { id: article.id } }" class="u-link-color">
+          {{ article.title }}
+        </nuxt-link>
+      </h5>
+      <p class="text-muted">{{ article.posted_at }}</p>
     </div>
   </div>
 </template>
