fe27f3d350300e32b41cd8ca9d0840a52ec07883
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-07-12 12:34:02 +0900

    add フッターにGAの文言を追記

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
   <footer class="l-footer">
-    <div class="text-dark py-3">
-      &copy; 2020 Hiroki Kawaguchi, Built with Nuxt.js
+    <div class="py-3">
+      <p class="text-dark">&copy; 2020 Hiroki Kawaguchi, Built with Nuxt.js</p>
+      <p class="text-dark">This site uses Google Analytics.</p>
     </div>
   </footer>
 </template>
