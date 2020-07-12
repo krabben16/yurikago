fa5604da236b2dccd0e9b4043d110f2c707cce9a
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-07-12 19:18:31 +0900

    fix #120 GitHubリンクをクリックしたときGAにデータを送信する

--- components/Header.vue
+++ components/Header.vue
@@ -46,16 +46,14 @@
           <nuxt-link :to="{ name: 'arigato' }" class="nav-link">Arigato</nuxt-link>
         </li>
       </ul>
-      <ul class="navbar-nav">
-        <li class="nav-item c-github-icon">
-          <a :href="githubUrl" target="_blank">
-            <picture>
-              <source srcset="~/assets/images/common/GitHub-Mark-32px.webp" type="image/webp" />
-              <img src="~/assets/images/common/GitHub-Mark-32px.png" alt="GitHub" />
-            </picture>
-          </a>
-        </li>
-      </ul>
+      <div class="c-github-icon">
+        <a :href="githubUrl" @click="$ga.event('header', 'click', 'github')" target="_blank">
+          <picture>
+            <source srcset="~/assets/images/common/GitHub-Mark-32px.webp" type="image/webp" />
+            <img src="~/assets/images/common/GitHub-Mark-32px.png" alt="GitHub" />
+          </picture>
+        </a>
+      </div>
     </div>
   </nav>
 </template>
