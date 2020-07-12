82af79134d6cea657c6657492da0a000b7a62687
Hiroki Kawaguchi <57534374+w9uZDFEYnT@users.noreply.github.com>
2020-07-12 16:47:40 +0900

    fix #121 エラーページのレイアウトを修正

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
-    <div class="l-error">
-      <h2 v-if="error.statusCode === 404">404 Not Found</h2>
-      <h2 v-else>エラーが発生しました</h2>
-      <div>
-        <pre class="c-error-text">
+  <div class="l-error">
+    <h2 v-if="error.statusCode === 404">404 Not Found</h2>
+    <h2 v-else>エラーが発生しました</h2>
+    <div class="ml-3">
+      <pre class="c-ascii-art">
         ∧ ∧
         ﾊ丿ﾊ丿
       ／￣￣＼
@@ -17,8 +16,7 @@
   ｜      ｜  ヽ|
    ＼ ノ   ＼ ノ|
 ￣￣￣￣￣￣￣￣￣￣
-        </pre>
-      </div>
+      </pre>
     </div>
   </div>
 </template>
