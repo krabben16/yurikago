80671265fcc33055f79fefb21cf016001ba186ec
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-07-14 20:54:13 +0900

    fix 記事内の画像をサムネイル表示する

--- components/Markdown.vue
+++ components/Markdown.vue
@@ -54,7 +54,7 @@ export default {
     // WebP画像を表示する
     renderer.image = (href, title, text) => {
       const webp = href.replace("png", "webp")
-      return `<picture><source srcset="${webp}" type="image/webp"><img src="${href}" alt="${text}"></picture>`
+      return `<picture><source srcset="${webp}" type="image/webp"><img src="${href}" alt="${text}" class="img-thumbnail"></picture>`
     }
 
     // 外部リンクを別タブで開く
