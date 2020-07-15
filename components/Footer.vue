ad3307f6224bfd1c4272eb68823ab8f92102ff22
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-07-15 21:24:17 +0900

    improve フッターのカラーテーマを変更する

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-  <footer class="l-footer bg-dark mt-5 py-3">
+  <footer class="l-footer mt-5 py-3">
     <a :href="githubUrl" target="_blank" @click="$ga.event('header', 'click', 'github')">
-      <div class="text-light my-3">
+      <div class="text-dark my-3">
         <i class="fab fa-github fa-3x" />
       </div>
     </a>
-    <div class="text-light">&copy; 2020 Hiroki Kawaguchi, Built with Nuxt.js</div>
-    <div class="text-light">This site uses Google Analytics.</div>
+    <div class="text-dark">&copy; 2020 Hiroki Kawaguchi, Built with Nuxt.js</div>
+    <div class="text-dark">This site uses Google Analytics.</div>
   </footer>
 </template>
 
