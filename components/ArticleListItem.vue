70d1519bfa694399af40b6b6b7e73936452d6222
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-07-18 17:34:22 +0900

    improve 記事カードの角を丸くする

--- components/ArticleListItem.vue
+++ components/ArticleListItem.vue
@@ -2,12 +2,12 @@
   <div class="p-card py-2" @mouseover="mouseOverAction" @mouseleave="mouseLeaveAction">
     <nuxt-link :to="{ name: 'articles-id', params: { id: article.id } }" class="u-link-color">
       <div class="card shadow-sm">
-        <div v-if="article.thumbnail" class="text-center py-5 text-dark" :class="article.thumbnail.bgClass">
+        <div v-if="article.thumbnail" class="text-center py-5 text-dark rounded-top" :class="article.thumbnail.bgClass">
           <span>
             <i class="p-card__image" :class="article.thumbnail.iconClass"></i>
           </span>
         </div>
-        <div class="card-body">
+        <div class="card-body rounded-bottom">
           <h5 class="card-title p-card__title" :class="{ 'u-underline': isMouseOver === true }">
             {{ article.title }}
           </h5>
