c373b91771f974a56b4b330883a41922b239c6af
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-07-21 20:04:18 +0900

    remove ありがとうページを削除

--- pages/arigato/index.vue
+++ pages/arigato/index.vue
@@ -1,67 +1,10 @@
-<template>
-  <div class="container-fluid bg-white">
-    <div class="row py-5">
-      <div class="col-12 col-sm-5 mx-sm-auto">
-        <Markdown :markdown-content="content" />
-      </div>
-    </div>
-  </div>
-</template>
-
 <script>
-import { mapActions } from "vuex"
-import Markdown from "~/components/Markdown.vue"
-import markdownSource from "~/assets/markdown/arigato.md"
-
 export default {
-  components: {
-    Markdown
-  },
-  created() {
-    this.content = markdownSource
-
-    // TDK
-    this.title = "ありがとうございます"
-    this.description = "利用させていただいているライブラリやフレームワークなどをまとめたページです。"
-
-    this.breadcrumbItemList = [
-      {
-        name: "トップページ",
-        path: "/"
-      },
-      {
-        name: this.title,
-        path: "/arigato"
-      }
-    ]
-  },
-  mounted() {
-    // パンくず
-    this.changeBreadcrumbItemList(this.breadcrumbItemList)
-  },
-  methods: {
-    ...mapActions("breadcrumb", ["changeBreadcrumbItemList"])
-  },
-  head() {
-    return {
-      title: this.title,
-      meta: [
-        {
-          "description": this.description
-        }
-      ],
-      script: [
-        // 構造化マークアップ
-        {
-          hid: "breadcrumbSchema",
-          innerHTML: this.$getBreadcrumbSchema(this.breadcrumbItemList),
-          type: "application/ld+json"
-        }
-      ],
-      __dangerouslyDisableSanitizersByTagID: {
-        breadcrumbSchema: ["innerHTML"]
-      }
-    }
+  /**
+   * NOTE: ページをクローズしてトップページへリダイレクトする
+   */
+  asyncData({ redirect }) {
+    return redirect(301, "/")
   }
 }
 </script>
