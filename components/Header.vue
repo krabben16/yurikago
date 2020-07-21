776d6b3c535c418752fd319a60acc1a153decb3c
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-07-21 20:10:59 +0900

    fix ヘッダーからありがとうページの導線を削除

--- components/Header.vue
+++ components/Header.vue
@@ -3,23 +3,5 @@
     <nuxt-link :to="{ name: 'index' }" class="navbar-brand">
       Yurikago Blog
     </nuxt-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon" />
-    </button>
-    <div id="navbarSupportedContent" class="collapse navbar-collapse">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <nuxt-link :to="{ name: 'arigato' }" class="nav-link">Arigato</nuxt-link>
-        </li>
-      </ul>
-    </div>
   </nav>
 </template>
