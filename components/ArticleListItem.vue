49a54cbce00f6748819d08c1033ec3e1a7a54249
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-07-23 09:22:03 +0900

    improve タグアイコンを表示する

--- components/ArticleListItem.vue
+++ components/ArticleListItem.vue
@@ -5,9 +5,21 @@
         {{ article.title }}
       </h2>
     </nuxt-link>
-    <div class="text-muted">
-      <i class="far fa-clock" />
+    <div class="mt-3">
+      <i class="far fa-clock mr-1" />
       {{ article.posted_at }}
+    </div>
+    <div class="mt-2">
+      <i class="fas fa-tag mr-1" />
+      <!-- NOTE: 二個目のタグからマージンを設定する -->
+      <nuxt-link
+        v-for="(tag, i) in article.tags"
+        :key="tag.id"
+        :to="{ name: 'articles-tag-id', params: { id: tag.id } }"
+        :class="{ 'ml-2': i > 0 }"
+      >
+        {{ tag.name }}
+      </nuxt-link>
     </div>
   </div>
 </template>
