e777adaf991cd5d1a04a095f1117ddad4a2c0a73
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-07-24 23:12:58 +0900

    fix 構造化データの画像を変更する

--- plugins/schema/article.js
+++ plugins/schema/article.js
@@ -1,3 +1,11 @@
+/**
+ * 記事
+ * https://developers.google.com/search/docs/data-types/article?hl=ja
+ *
+ * ロゴ
+ * https://developers.google.com/search/docs/data-types/logo?hl=ja
+ */
+
 import Vue from "vue"
 import { getJoinedTagsName } from "~/plugins/tags.js"
 
@@ -12,7 +20,7 @@ Vue.prototype.$getArticleSchema = article => {
       "@id": `${process.env.FRONT_URL}/articles/${article.id}`
     },
     "headline": article.title,
-    "image": [`${process.env.FRONT_URL}/icon.jpg`],
+    "image": [`${process.env.FRONT_URL}/16x9.jpg`],
     "datePublished": `${article.posted_at}T00:00:00+09:00`,
     "dateModified": `${article.posted_at}T00:00:00+09:00`,
     "author": {
