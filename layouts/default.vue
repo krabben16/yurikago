43fda7a05a245a2117b3029ea0d63b30af56ee16
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-09-06 16:24:27 +0900

    add githubのリンクをヘッダーに追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,20 +2,17 @@
   <div>
     <Header />
     <nuxt />
-    <Social />
     <Footer />
   </div>
 </template>
 
 <script>
 import Header from "~/components/Header.vue"
-import Social from "~/components/Social.vue"
 import Footer from "~/components/Footer.vue"
 
 export default {
   components: {
     Header,
-    Social,
     Footer
   }
 }
