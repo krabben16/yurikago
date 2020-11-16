24936c91f9a42e9208ddbe676cb01ace6163049a
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-11-16 23:39:07 +0900

    fix: change article list layout

--- pages/tags/_id.vue
+++ pages/tags/_id.vue
@@ -4,8 +4,8 @@
       <Placeholder />
     </template>
     <template v-else>
-      <div class="row">
-        <div class="col-12 col-sm-6 mx-sm-auto py-5">
+      <div class="row py-5">
+        <div class="col-12 col-sm-5 mx-sm-auto">
           <ArticleList :articles="articles" :title="`タグ: ${tag.name}`" />
         </div>
       </div>
