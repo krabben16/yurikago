8d944a928acee20bd89fd3513407413f34a8e7ee
Hiroki Kawaguchi <kawakanu@gmail.com>
2020-12-02 02:16:42 +0900

    add: prism-themes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -128,6 +128,11 @@ const config: NuxtConfig = {
 
   content: {
     nestedProperties: ['tags.id'],
+    markdown: {
+      prism: {
+        theme: 'prism-themes/themes/prism-nord.css',
+      },
+    },
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
