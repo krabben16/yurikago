fa5a49fcd94881e921a3c8cbf6d1085775861707
Hiroki Kawaguchi <kawakanu@gmail.com>
2021-01-12 20:59:01 +0900

    Revert "remove: fetchTotalArticleCount"
    This reverts commit b8e8ef617666eedf1a6afa32bf090c49cf2de61f.

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,27 @@
 <template>
-  <div class="min-vh-100 px-3 px-sm-5 py-5 bg-white rounded">
-    <ArticleList :articles="articles" />
+  <div>
+    <template v-if="$fetchState.pending || $fetchState.error">
+      <Placeholder />
+    </template>
+    <template v-else>
+      <div class="min-vh-100 px-3 px-sm-5 py-5 bg-white rounded">
+        <ArticleList :articles="articles" />
+      </div>
+    </template>
   </div>
 </template>
 
 <script lang="ts">
 import {
   defineComponent,
-  useAsync,
+  ref,
   useContext,
+  useFetch,
   useMeta,
 } from '@nuxtjs/composition-api'
+import { CommonHead } from '~/interfaces/Head'
 import { BreadcrumbSchema } from '~/interfaces/Schema'
+import { ContentArticleListItem } from '~/interfaces/Content'
 import { createHeadObject } from '~/lib/head/common'
 import { ContentFunctions as cf } from '~/lib/content/article'
 
@@ -21,32 +31,48 @@ export default defineComponent({
   setup() {
     const { $content, route } = useContext()
 
-    const articles = useAsync(async () => await cf.fetchAllArticles($content))
+    const articles = ref<ContentArticleListItem[]>()
+    const meta = ref<CommonHead>()
 
-    useMeta(() => {
-      const title = 'トップページ'
-      const description = `${process.env.SITE_OWNER}の技術ブログです。`
-      const path = route.value.path
-
-      const breadcrumbSchema: BreadcrumbSchema = {
-        items: [
-          {
-            name: title,
-            path: route.value.path,
-          },
-        ],
+    useFetch(async () => {
+      async function fetchArticles() {
+        return await cf.fetchAllArticles($content)
       }
 
-      return createHeadObject({
-        title,
-        description,
-        path,
-        breadcrumbSchema,
-      })
+      function fetchMeta(): CommonHead {
+        const title = 'トップページ'
+        const description = `${process.env.SITE_OWNER}の技術ブログです。`
+        const path = route.value.path
+
+        const breadcrumbSchema: BreadcrumbSchema = {
+          items: [
+            {
+              name: title,
+              path: route.value.path,
+            },
+          ],
+        }
+
+        return {
+          title,
+          description,
+          path,
+          breadcrumbSchema,
+        }
+      }
+
+      articles.value = await fetchArticles()
+      meta.value = fetchMeta()
+    })
+
+    useMeta(() => {
+      if (!meta.value) return {}
+      return createHeadObject(meta.value)
     })
 
     return {
       articles,
+      meta,
     }
   },
 })
