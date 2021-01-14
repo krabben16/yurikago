5bec4417ecfe51d5ba4c81034ef5e503fa33ce04
Hiroki Kawaguchi <kawakanu@gmail.com>
2021-01-15 00:38:52 +0900

    rename: Header.vue to Nav.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-10 mx-sm-auto">
-        <Header />
+        <Nav />
         <nuxt />
         <Footer />
       </div>
